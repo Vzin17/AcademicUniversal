0bbbe93bdcb13e43f4c37ab70bf59be102f6925e
alinykrebss <alinykrebs@gmail.com>
2025-07-30 21:29:15 -0300

    saiba_como_ser_atendido
    tentei fazer a aba de ajuda

---
+++ src/Pages/saiba_como_ser_atendido/saiba_como_ser_atendido.js
@@ -0,0 +1,17 @@
+import "./saiba_como_ser_atendido.css";
+
+function Ajuda() {
+    return(
+        <h2>
+            como funcionam nossos atendimentos?
+        </h2>
+    
+    );
+    
+    
+}
+
+
+
+
+export default Ajuda;
