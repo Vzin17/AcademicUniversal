5986bdcb74fcc5662f428718de15ee5bc7d1b1f5
alinykrebss <alinykrebs@gmail.com>
2025-08-20 22:32:56 -0300

    foi, algo ta melhor
    tá indo

--- src/Pages/Denuncia.jsx
+++ src/Pages/Denuncia.jsx
@@ -1,19 +1,4 @@
-/*
-import './CSS_Pgs/Denuncia.css';
 
-
-function Denuncia() {
-  return (
-    <div>
-      <h1>Página de Denúncia Anônima</h1>
-      <p>Preencha os campos abaixo para enviar sua denúncia.</p>
-    </div>
-  );
-}
-
-export default Denuncia;
-*/
-import React from 'react';
 import './CSS_Pgs/Denuncia.css';
 
 function Denuncia() {
